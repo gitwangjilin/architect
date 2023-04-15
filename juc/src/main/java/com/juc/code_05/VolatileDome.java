ddc614207463982f018088e9379254da3acc8850
admin <www.461189416@qq.com>
2023-04-15 15:46:21 +0800

    volatile有两大特性，一是可见性，二是有序性，禁止指令重排序，其中可见性就是可以让线程之间进行通信

---
+++ juc/src/main/java/com/juc/code_05/VolatileDome.java
@@ -0,0 +1,28 @@
+package com.juc.code_05;
+
+public class VolatileDome {
+    private static volatile boolean flag = true;
+
+    public static void main(String[] args) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true) {
+                    if (flag) {
+                        System.out.println("trun on");
+                        flag = false;
+                    }
+                }
+            }
+        }).start();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                if (!flag) {
+                    System.out.println("trun off");
+                    flag = true;
+                }
+            }
+        }).start();
+    }
+}
