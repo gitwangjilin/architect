b68a5fb092c367101a96343313fc23b1c7209671
admin <www.461189416@qq.com>
2023-04-15 14:27:02 +0800

    伪共享相差十倍

---
+++ juc/src/main/java/com/juc/code_02/FalseSharingTest.java
@@ -0,0 +1,36 @@
+package com.juc.code_02;
+
+public class FalseSharingTest {
+    public static void main(String[] args) throws InterruptedException {
+        testPointer(new Pointer());
+    }
+
+    private static void testPointer(Pointer pointer) throws InterruptedException {
+        long start = System.currentTimeMillis();
+        Thread t1 = new Thread(() -> {
+            for (int i = 0; i < 100000000; i++) {
+                pointer.x++;
+
+            }
+        });
+        Thread t2 = new Thread(() -> {
+            for (int i = 0; i < 100000000; i++) {
+                pointer.y++;
+
+            }
+        });
+        t1.start();
+        t2.start();
+        t1.join();
+        t2.join();
+        System.out.println(pointer.x + "," + pointer.y);
+        System.out.println(System.currentTimeMillis() - start);
+    }
+}
+
+class Pointer {
+    volatile long x;
+    //为共享相差十倍
+    long p1, p2, p3, p4, p5, p6, p7;
+    volatile long y;
+}
